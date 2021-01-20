826387f23c4eba6a4935605cb620a9986f73d1b0
Laura Izabel <lauraizabel2563@gmail.com>
2021-01-20 15:10:47 -0300

    port env file

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,18 @@
-const express = require("express")
-var cors = require('cors')
-const bodyParser = require("body-parser")
-const tasks = require("./routes/tasks")
-const user = require("./routes/user")
+const express = require("express");
+var cors = require("cors");
+const bodyParser = require("body-parser");
+const tasks = require("./routes/tasks");
+const user = require("./routes/user");
 //const db = require("../models/pool")
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/user", user)
-app.use("/tasks", tasks)
+app.use("/user", user);
+app.use("/tasks", tasks);
 
-app.listen(3000, () => {
-  console.log("Running on port 3000.")
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Running on port 3000.");
+});
